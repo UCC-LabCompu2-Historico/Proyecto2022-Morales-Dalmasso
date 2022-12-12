62ef0f15c0c6efa5a21e8a10b9dfd7c652136d38
bautidalmasso <bautidalmasso918@gmail.com>
2022-12-12 18:58:51 -0300

    Actualizacion2 del proyecto

--- jscv.js
+++ jscv.js
@@ -5,75 +5,76 @@
  * @param  finalAngle  -   Variable Contador de la secuencia circular
  * Tamaño de letra en canvas
  */
-function GrafCripto(){
+function GrafCripto() {
     let canvas = document.getElementById('canvas');
     let context = canvas.getContext('2d');
 
     let finalAngle = 0;
 
-    setInterval(function (){
+    setInterval(function () {
         context.beginPath();
-        context.arc(180,200,96,0,Math.PI * 2 );
+        context.arc(180, 200, 96, 0, Math.PI * 2);
         context.lineWidth = 2;
         context.strokeStyle = "#faf7f7";
         context.stroke();
-    },100 / 4)
+    }, 100 / 4)
 
-    setInterval(function (){
+    setInterval(function () {
         finalAngle += 0.001;
         context.beginPath();
-        context.arc(180,200,100,0,Math.PI * finalAngle);
+        context.arc(180, 200, 100, 0, Math.PI * finalAngle);
         context.strokeStyle = "#dcc217";
         context.stroke();
-    },1000 / 0.5)
+    }, 1000 / 0.5)
 
-    setInterval(function (){
+    setInterval(function () {
         finalAngle += 0.01;
         context.beginPath();
-        context.arc(400,200,100,0,Math.PI * finalAngle);
+        context.arc(400, 200, 100, 0, Math.PI * finalAngle);
         context.strokeStyle = "#008000";
         context.stroke();
-    },1000 / 5)
-    setInterval(function (){
+    }, 1000 / 5)
+    setInterval(function () {
         context.beginPath();
-        context.arc(400,200,96,0,Math.PI * 2 );
+        context.arc(400, 200, 96, 0, Math.PI * 2);
         context.lineWidth = 2;
         context.strokeStyle = "#faf7f7";
         context.stroke();
-    },100 / 2)
+    }, 100 / 2)
 
-    setInterval(function (){
+    setInterval(function () {
         finalAngle += 0.001;
         context.beginPath();
-        context.arc(620,200,100,0,Math.PI * finalAngle);
+        context.arc(620, 200, 100, 0, Math.PI * finalAngle);
         context.strokeStyle = "#cb1c3c";
         context.stroke();
-    },1000 / 2)
-    setInterval(function (){
+    }, 1000 / 2)
+    setInterval(function () {
         context.beginPath();
-        context.arc(620,200,96,0,Math.PI * 2 );
+        context.arc(620, 200, 96, 0, Math.PI * 2);
         context.lineWidth = 2;
         context.strokeStyle = "#faf7f7";
         context.stroke();
-    },100 / 2)
+    }, 100 / 2)
 
-    context.font= "40px American Catain";
-    context.fillStyle="silver";
-    context.fillText("BTC",145,205)
+    context.font = "40px American Catain";
+    context.fillStyle = "silver";
+    context.fillText("BTC", 145, 205)
 
-    context.font= "40px American Catain";
-    context.fillStyle="silver";
-    context.fillText("LTC",365,205)
+    context.font = "40px American Catain";
+    context.fillStyle = "silver";
+    context.fillText("LTC", 365, 205)
 
-    context.font= "40px American Catain";
-    context.fillStyle="silver";
-    context.fillText("ETH",585,205)
+    context.font = "40px American Catain";
+    context.fillStyle = "silver";
+    context.fillText("ETH", 585, 205)
 
-    context.font= "40px American Catain";
-    context.fillStyle="silver";
-    context.fillText("Mineria de Cripto",250,80)
+    context.font = "40px American Catain";
+    context.fillStyle = "silver";
+    context.fillText("Mineria de Cripto", 250, 80)
 
 }
+
 /**
  * Boton para realizar la mineria nuevamente
  * Nombre de la función reclamarcripto
@@ -83,11 +84,11 @@ function GrafCripto(){
  * @param {cargarDatos} - Declaracion de la funcionn
  */
 
-async function reclamarcripto(){
+async function reclamarcripto() {
 
     await Swal.fire({
-        title:"RECLAMASTE TUS CRIPTOMONEDAS",
-        text:"Seran enviadas a tu Wallet automaticamente",
+        title: "RECLAMASTE TUS CRIPTOMONEDAS",
+        text: "Seran enviadas a tu Wallet automaticamente",
         icon: "success",
         confirmButtonText: '<a href="mineria.html">Empecemos otra vez!</a>'
     });
@@ -95,46 +96,46 @@ async function reclamarcripto(){
 }
 
 
-function registristrarse(){
+function registristrarse() {
     let nombre, apellido, correo, password;
 
     //Creo 4 variables...Y luego debo obtener el valor de los campos, y guardarlo en variables
     nombre = document.getElementById("nombres").value;
-    apellido =document.getElementById("apellido").value;
-    correo =document.getElementById("correo").value;
-    password =document.getElementById("password").value;
+    apellido = document.getElementById("apellido").value;
+    correo = document.getElementById("correo").value;
+    password = document.getElementById("password").value;
     //hacer lo mismo con el resto de las variables
 
     //Si el nombre esta vacio, debe visualizarse un mensaje de error
-    if(!nombre){
+    if (!nombre) {
         alert("Debe completar el nombre para continuar!")
-    }else if(!apellido){
+    } else if (!apellido) {
         alert("Debe completar el apellido para continuar!")
-    }else if(!correo){
+    } else if (!correo) {
         alert("Debe completar el correo para continuar!")
-    }else if(!password){
+    } else if (!password) {
         alert("Debe completar el password para continuar!")
-    }
-    else{
+    } else {
         localStorage.setItem("storageNombre", nombre);
         localStorage.setItem("storageApellido", apellido);
         localStorage.setItem("storageCorreo", correo);
         localStorage.setItem("storagePassword", password);
     }
 
-    if(nombre || apellido || correo || password){ //y apellido y correo y pass están completos
-        location.href="mineria.html";
+    if (nombre || apellido || correo || password) { //y apellido y correo y pass están completos
+        location.href = "mineria.html";
     }
 
 }
+
 function cargarDatos() {
     let nombre, apellido, correo, password;
     nombre = localStorage.getItem("storageNombre");
     document.getElementById("nombre").innerHTML = nombre;
     apellido = localStorage.getItem("storageApellido");
-    document.getElementById("apellido").innerHTML =apellido;
+    document.getElementById("apellido").innerHTML = apellido;
     correo = localStorage.getItem("storageCorreo");
-    document.getElementById("correo").innerHTML =correo;
-    password =localStorage.getItem("storagePassword");
-    document.getElementById("password").innerHTML =password;
+    document.getElementById("correo").innerHTML = correo;
+    password = localStorage.getItem("storagePassword");
+    document.getElementById("password").innerHTML = password;
 }
